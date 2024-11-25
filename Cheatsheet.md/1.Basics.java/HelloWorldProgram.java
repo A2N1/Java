25f92037de8de1cafd55dee3980fad7df2449af0
AlphaNiro <155444561+A2N1@users.noreply.github.com>
2024-11-25 18:14:00 +0100

    Create HelloWorldProgram.java

---
+++ Cheatsheet.md/1.Basics.java/HelloWorldProgram.java
@@ -0,0 +1,13 @@
+/**
+ * This is a simple HelloWorld program.
+ */
+public class HelloWorld {
+    /**
+     * Main method, entry point of the application.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
